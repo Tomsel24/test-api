9835e509a4b87847292f07afcc7eab89129263cd
Kevin <96058589+Tomsel24@users.noreply.github.com>
2024-08-18 18:26:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Función para generar un número aleatorio de 10 dígitos
-function generateRandomNumber() {
-    return Math.floor(1000000000 + Math.random() * 9000000000);
+app.use(express.json());
+
+
+// Función para generar un número aleatorio de 10 dígitos que no se repita
+function generateUniqueRandomNumber() {
+    let randomNumber;
+    do {
+        randomNumber = Math.floor(1000000000 + Math.random() * 9000000000);
+    } while (generatedNumbers.has(randomNumber));
+
+    // Agregar el número al conjunto para evitar repeticiones
+    generatedNumbers.add(randomNumber);
+
+    return randomNumber;
 }
 
 // Ruta para obtener el número aleatorio
@@ -15,5 +26,5 @@ app.get('/random-number', (req, res) => {
 
 // Iniciar el servidor
 app.listen(port, () => {
-    console.log(`API de números aleatorios escuchando en http://localhost:3000`);
+    console.log(`API de números aleatorios escuchando en http://localhost:${port}`);
 });
